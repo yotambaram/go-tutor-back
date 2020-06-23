27b3901b5ff5e3f27c3982a89cdd8f6cae5c5b0e
yotamba12 <yotambaram@gmail.com>
2020-06-23 12:59:04 -0700

    cors origin update

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use(cors({
   //origin: ["http://localhost:3000"], 
-  origin: ["https://gotutor.herokuapp.com/"],
+  origin: ["https://gotutor.herokuapp.com"],
   credentials:true
 })); 
 
